1f99a0c5f716875e5b59f014ac530bfc459e1598
williams-esthera <williams.esthera@gmail.com>
2025-02-28 15:43:07 -0500

    added button functionality, clear button, and edge case 1&2 can't start with operator, can't add operator next to operator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,32 +36,112 @@ class _MyHomePageState extends State<MyHomePage> {
   Color accent1 = Color(0xFFA04668);
   Color accent2 = Color.fromARGB(255, 100, 24, 60);
   Color text = Color(0xFF0C1713);
-  int _counter = 0;
+  Color text2 = Color.fromARGB(255, 250, 254, 252);
+  String calculations = "";
+  String error = "";
 
-  void _incrementCounter() {
+  List nums = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
+
+  void _gridOneOperators(String op) {
     setState(() {
-      _counter++;
+      if (nums.contains(op)) {
+         error = "";
+        calculations += op;
+      } else {
+        switch (op) {
+          case 'C':
+            calculations = "";
+            error = "";
+            break;
+          case '=':
+            if (checkEnter()) {
+              error = "";
+              enter(calculations);
+            }
+            break;
+        }
+      }
     });
+  }
+
+  void _gridTwoOperators(String op) {
+    setState(() {
+      if(check2()){
+         error = "";
+        calculations += " $op ";
+      }
+    });
+  }
+
+  bool checkEnter() {
+    //check if the last char in calc is a number 
+    if(nums.contains(getLastChar())){
+      error = "";
+      return true;
+    }
+    //else return false
+    //error message: can't end equation with an operator
+
+    error = "Can't end equation with an operator";
+    return false;
+  }
+
+  bool check2 (){
+    //if calculations is empty return false
+    if (calculations.isEmpty){
+      error = "Cannot start with an operator";
+      return false;
+    }
+
+
+    //get last char in calculation
+      
+    //this will check if the last entered character is a number
+    //if the last char is a number then return true
+    if(nums.contains(getLastChar())){
+      return true;
+    }
+    error = "Cannot place operator next to operator";
+    return false;
+  }
+
+  void enter(String equation) {
+    // Calculate and then return
+    Parser parser = Parser();
+    Expression expression = parser.parse(equation);
+    double result = expression.evaluate(EvaluationType.REAL, ContextModel());
+    error = "$result";
+    calculations = "";
+  }
+
+  String getLastChar(){
+    int calcLength = calculations.length;
+    String lastNum = calculations.substring(calcLength - 1);
+    return lastNum;
   }
 
   @override
   Widget build(BuildContext context) {
+    List grid1 = ['9', '8', '7', '6', '5', '4', '3', '2', '1', 'C', '0', '='];
+    List grid2 = ['+', '-', '*', '/'];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: background2,
         title: Text(widget.title),
         titleTextStyle: TextStyle(
           fontFamily: 'BubbleFont', // Your custom font
-          fontSize: 30,
+          fontSize: 50,
+          fontWeight: FontWeight.bold,
           color: text,
         ),
       ),
       backgroundColor: background,
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start, // Align all children to the start
+        mainAxisAlignment:
+            MainAxisAlignment.start, // Align all children to the start
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          // First Expanded for 'Calculations'
+          // First Expanded for Calculations + Error messages
           Expanded(
             flex: 1,
             child: Container(
@@ -69,44 +150,61 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 200,
                 width: double.infinity,
                 child: Center(
-                  child: Text(
-                    'Calculations',
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Text(
+                    error,
+                    style: TextStyle(fontSize: 40, color: text2),
+                  ),
+                  Text(
+                    calculations,
                     style: TextStyle(fontSize: 30, color: text),
                   ),
+                  ],
+                ),
                 ),
               ),
             ),
           ),
-          
-          // Second Expanded for grid buttons with Padding
+
+          // Second Expanded for Numbers, Clear button, and Equqal button
           Expanded(
             flex: 3,
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0), // Add vertical padding
+              padding: const EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 70.0,
+              ), // Add vertical padding
               child: SizedBox(
                 height: 100,
                 width: double.infinity,
                 child: Center(
                   child: GridView.builder(
-                    itemCount: 12,
+                    itemCount: grid1.length,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 3,
                       childAspectRatio: 1,
                       mainAxisSpacing: 8, // Space between rows
                       crossAxisSpacing: 8, // Space between cols
                     ),
+
                     itemBuilder: (context, index) {
                       return Container(
                         height: 5,
                         width: 5,
                         child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                            primary: accent1,
+                            backgroundColor: accent1,
                           ),
-                          onPressed: () {},
-                          child: Text(
-                            '#',
-                            style: TextStyle(fontSize: 30, color: text),
+                          onPressed: () {
+                            _gridOneOperators(grid1[index]);
+                          },
+                          child: Center(
+                            child: Text(
+                              grid1[index],
+                              style: TextStyle(fontSize: 50, color: text),
+                            ),
                           ),
                         ),
                       );
@@ -116,36 +214,46 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-          
-          // Third GridView at the bottom
+
+          // Third GridView for operators
           Container(
             height: 200, // Set the height as needed
             width: double.infinity,
-            child: Center(
-              child: GridView.builder(
-                itemCount: 4,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  childAspectRatio: 1,
-                  mainAxisSpacing: 8, // Space between rows
-                  crossAxisSpacing: 8, // Space between cols
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20.0,
+                horizontal: 20.0,
+              ),
+              child: Center(
+                child: GridView.builder(
+                  itemCount: grid2.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    childAspectRatio: 1,
+                    mainAxisSpacing: 8, // Space between rows
+                    crossAxisSpacing: 8, // Space between cols
+                  ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 5,
+                      width: 5,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: accent2,
+                        ),
+                        onPressed: () {
+                          _gridTwoOperators(grid2[index]);
+                        },
+                        child: Center(
+                          child: Text(
+                            grid2[index],
+                            style: TextStyle(fontSize: 50, color: text2),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 5,
-                    width: 5,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: accent2,
-                      ),
-                      onPressed: () {},
-                      child: Text(
-                        '+',
-                        style: TextStyle(fontSize: 30, color: text),
-                      ),
-                    ),
-                  );
-                },
               ),
             ),
           ),
